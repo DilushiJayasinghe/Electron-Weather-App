e92729fb488c410ba58c4f66c096fb4181f660b8
DilushiJayasinghe <dilushijayasinghe02@gmail.com>
2024-04-02 00:09:09 +0530

    Add fetch function

--- src/script.js
+++ src/script.js
@@ -1,11 +1,61 @@
 const container = document.querySelector('.container');
-const search = document.querySelector('.container');
-const weatherBox = document.querySelector('.container');
-const weatherDetails = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
 
-search.addEventListener('click', () =>{
+search.addEventListener('click', () => {
 
     const APIKey = 'f658d43aebaf69430bbd11ab71b88cbf';
-    
+    const city = document.querySelector('.search-box input').ariaValueMax;
 
-})
+    if (city = '')
+        return;
+
+    fetch('https://pro.openweathermap.org/data/2.5/forecast/climate?q=${city}&units=metric&appid=${APIkey}').then(response => response.json()).then(json => {
+
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+
+            case 'Haze':
+                image.src = 'images/mist.png';
+                break;
+
+            default:
+                image.scr = 'images/cloud.png';
+
+        }
+
+        // temperature.innerHTML = ${parseInt(json.main.temp)}<span>°C</span>;
+        // temperature.innerHTML = ${json.weather[0].description};
+        // temperature.innerHTML = ${};
+        // temperature.innerHTML = ${};
+
+    });
+
+
+
+});
