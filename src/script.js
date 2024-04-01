643381f9b1771fa350cd83e3d9dfa6b683b47890
DilushiJayasinghe <dilushijayasinghe02@gmail.com>
2024-04-01 23:43:32 +0530

    Add open-weather API

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.container');
+const weatherBox = document.querySelector('.container');
+const weatherDetails = document.querySelector('.container');
+
+search.addEventListener('click', () =>{
+
+    const APIKey = 'f658d43aebaf69430bbd11ab71b88cbf';
+    
+
+})
